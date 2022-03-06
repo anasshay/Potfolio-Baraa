c59d1cec6383d8f0d834f2c8c782a3649c2c4c89
anasshay <84935610+anasshay@users.noreply.github.com>
2022-03-06 20:07:39 +0200

    Update about-me.js

--- team-1/controller/about-me.js
+++ team-1/controller/about-me.js
@@ -21,21 +21,33 @@ class Controller {
   edit(req, res, next) {
     let content = req.body;
     AboutMe.find({}, (err, response) => {
-      let aboutMeId = response[0]._id;
-    AboutMe.updateOne({_id: aboutMeId}, {
-      $set: content
-    }, (err, response2) => {
-      if (err) return next(err)
-      res.status(200).send({ success: true, response2 })
-    }
-    )
-  })
+      if (response.length == 0) {
+        let newContent = new AboutMe({
+          name: "undefined",
+          description: "undefined",
+          content: "undefined",
+        });
+        newContent.save();
+      }
+      else {
+        let aboutMeId = response[0]._id;
+        AboutMe.updateOne(
+          { _id: aboutMeId },
+          {
+            $set: content,
+          },
+          (err, response2) => {
+            if (err) return next(err);
+            res.status(200).send({ success: true, response2 });
+          }
+        );
+      }
+    });
   }
 
   // edit(req, res, next) {
   //   res.send(req.body)
   // }
-
 }
 
 const controller = new Controller();
